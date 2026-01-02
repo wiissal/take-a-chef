68f24560d485dbfb8ea26246586a20fa30a828df
wiissal <wissaloa1@gmail.com>
2026-01-02 12:36:19 +0100

    feat: create Chef  model and sync with PostgreSQL database

---
+++ backend/src/models/Chef.js
@@ -0,0 +1,55 @@
+const {DataTypes} = require ('sequelize');
+const {sequelize} = require('../config/database');
+const User = require('./User');
+
+const Chef = sequelize.define('Chef',{
+  id:{
+    type: DataTypes.INTIGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_id:{
+    type: DataTypes.INTIGER,
+    allowNull: false,
+    unique: true,
+    references:{
+      model: 'users',
+      key:'id'
+    }
+  },
+  bio:{
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  photo:{
+    type: DataTypes.STRING(500),
+    allowNull: true
+  },
+  speciality:{
+    type: DataTypes.STRING(100),
+    allowNull: true
+  },
+  rating:{
+    type: DataTypes.DECIMAL(3,2),
+    defaultValue: 0.00,
+    validate:{
+      min:0,
+      max:5
+    }
+  },
+  total_reviews:{
+      type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+},{
+  tableName: 'chefs',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+//define relationships
+Chef.belongsTo(User,{foreignKey: ' user_id', as: 'user'});
+User.hasOne(Chef, {foreignKey: 'user_id', as: 'chef'});
+
+module.exports= Chef
