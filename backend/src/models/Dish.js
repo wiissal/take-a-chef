ac3edbaa267d22be5ae729ca041f28e46b86967e
wiissal <wissaloa1@gmail.com>
2026-01-02 13:24:53 +0100

    feat: create Dish model and sync with PostgreSQL database

---
+++ backend/src/models/Dish.js
@@ -0,0 +1,49 @@
+const {DataTypes} = require ('sequelize');
+const { sequelize } = require ('../config/database');
+const {Chef} = require('./Chef');
+
+const Dish = sequelize.define('Dish', {
+  id:{
+    type: DataTypes.INTIGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  chef_id :{
+    type: DataTypes.INTIGER,
+    allowNull: false,
+    references:{
+      model: 'chefs',
+      key: 'id',
+    }
+  },
+  name:{
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  description : {
+    type: DataTypes.TEXT,
+    allowNull: true
+    },
+    price:{
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate:{
+        min: 0
+      }
+    }, 
+   image:{
+    type:  DataTypes.STRING(500),
+    allowNull: true
+   } 
+  },{
+      tableName: 'dishes',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+//define relationships
+Dish.belongsTo(Chef , {foreignKey: 'chef_id', as : 'chef'});
+Chef.hasMany(Dish, {foreignKey: 'chef_id', as : 'dishes'});
+
+module.exports= Dish;
