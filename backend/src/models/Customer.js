1789380e53dda5a3ca48b638f6d7e16fac4c7b2c
wiissal <wissaloa1@gmail.com>
2026-01-02 12:55:59 +0100

    feat: create Customer model and sync with PostgreSQL database

---
+++ backend/src/models/Customer.js
@@ -0,0 +1,34 @@
+const {DataTypes} = require ('sequelize');
+const {sequelize} = require ('../config/database');
+const User = require ('./User');
+
+const Customer = sequelize.define('Customer',{
+  id: {
+    type: DataTypes.INTIGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_id:{
+    type: DataTypes.INTIGER,
+    allowNull: false,
+    unique: true,
+    references:{
+      model: 'users',
+      key: 'id'
+    }
+  },
+  phone:{
+    type: DataTypes.STRING(20),
+    allowNull: true,
+  },
+},{
+  tableName: 'customers',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+Customer.belongsTo(User, { foreignKey: 'user_id', as: 'user'});
+User.hasOne(Customer, { foreignKey: 'user_id' , as : 'costumer'});
+
+module.exports = Customer;
